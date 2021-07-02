66489f6244fc4fc4b2d22e433d2c8a5a8d704743
chungkyungsook <tah05005@gmail.com>
2021-07-02 19:46:22 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEl      = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-//돋보기 아이콘있는 박스 클릭 시 input 요소로 포커스
-searchEl.addEventListener('click',()=>{
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',()=>{
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur',()=>{
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder','');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(() =>{
@@ -148,5 +129,3 @@ spyEls.forEach(function(spyEl){
 });
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
